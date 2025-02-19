befc30de66ce2cb44d7fcfcdafff4c1a699d7b10
sc-johanbecue <johan.becue@sitecore.com>
2025-02-20 00:02:21 +0100

    modified:   headapps/nextjs-starter/next-env.d.ts 	new file:   headapps/nextjs-starter/public/css/custom.css 	new file:   headapps/nextjs-starter/public/css/fonts/star.eot 	new file:   headapps/nextjs-starter/public/css/fonts/star.svg 	new file:   headapps/nextjs-starter/public/css/fonts/star.ttf 	new file:   headapps/nextjs-starter/public/css/fonts/star.woff 	new file:   headapps/nextjs-starter/public/css/skins/skin-corporate-10.css 	new file:   headapps/nextjs-starter/public/css/theme-blog.css 	new file:   headapps/nextjs-starter/public/css/theme-elements.css 	new file:   headapps/nextjs-starter/public/css/theme-shop.css 	new file:   headapps/nextjs-starter/public/css/theme.css 	new file:   headapps/nextjs-starter/public/img/arrows-dark.png 	new file:   headapps/nextjs-starter/public/img/arrows.png 	new file:   headapps/nextjs-starter/public/img/curved-border.svg 	new file:   headapps/nextjs-starter/public/img/flags.png 	new file:   headapps/nextjs-starter/public/img/home/home-concept-dark.png 	new file:   headapps/nextjs-starter/public/img/home/home-concept-icons-dark.png 	new file:   headapps/nextjs-starter/public/img/home/home-concept-icons.png 	new file:   headapps/nextjs-starter/public/img/home/home-concept-item-dark.png 	new file:   headapps/nextjs-starter/public/img/home/home-concept-item.png 	new file:   headapps/nextjs-starter/public/img/home/home-concept.png 	new file:   headapps/nextjs-starter/public/img/icons/icons.png 	new file:   headapps/nextjs-starter/public/img/lazy.png 	new file:   headapps/nextjs-starter/public/img/map.png 	new file:   headapps/nextjs-starter/public/img/overlay-dot.png 	new file:   headapps/nextjs-starter/public/img/particle-dot.svg 	new file:   headapps/nextjs-starter/public/img/patterns/az_subtle.png 	new file:   headapps/nextjs-starter/public/img/patterns/worn_dots.png 	new file:   headapps/nextjs-starter/public/img/pencil-blue-line-2-dark.png 	new file:   headapps/nextjs-starter/public/img/pencil-blue-line-2.png 	new file:   headapps/nextjs-starter/public/img/pencil-blue-line-dark.png 	new file:   headapps/nextjs-starter/public/img/pencil-blue-line.png 	new file:   headapps/nextjs-starter/public/img/vertical-lines-light.svg 	new file:   headapps/nextjs-starter/public/img/vertical-lines.svg 	new file:   headapps/nextjs-starter/public/js/custom.js 	new file:   headapps/nextjs-starter/public/js/theme.init.js 	new file:   headapps/nextjs-starter/public/js/theme.js 	new file:   headapps/nextjs-starter/public/js/views/view.contact.js 	new file:   headapps/nextjs-starter/public/vendor/plugins/js/plugins.min.js 	new file:   headapps/nextjs-starter/src/Layout.original.tsx 	modified:   headapps/nextjs-starter/src/Layout.tsx 	new file:   headapps/nextjs-starter/src/components/SolutionEngineering/Header.tsx 	new file:   headapps/nextjs-starter/src/components/SolutionEngineering/HomeIntro.Tsx 	new file:   headapps/nextjs-starter/src/pages/_document.tsx 	modified:   headapps/nextjs-starter/tsconfig.json 	new file:   package-lock.json

---
+++ headapps/nextjs-starter/src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body data-plugin-page-transition>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

1d0eccb791f77d3c28778156b02cec6200544fb2
sc-johanbecue <johan.becue@sitecore.com>
2025-02-20 14:02:44 +0100

    modified:   headapps/nextjs-starter/src/Layout.tsx

--- headapps/nextjs-starter/src/Layout.tsx
+++ headapps/nextjs-starter/src/Layout.tsx
@@ -137,12 +137,14 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
           {route && <Placeholder name="headless-header" rendering={route} />}
         </header>
         <main>
-          <div role="main" className="main" id="content">
-            {route && <Placeholder name="headless-main" rendering={route} />}
+          <div role="main" className="main">
+            <div id="content">
+              {route && <Placeholder name="headless-main" rendering={route} />}
+            </div>
           </div>
         </main>
         <footer id="footer" className="bg-color-light border-0 pt-5 mt-0">
-          {route && <Placeholder name="headless-footer" rendering={route} />}
+          <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
         </footer>
       </div>
       <a

b593eeb5b338cac4c99b1f33765af0ee8f19330d
sc-johanbecue <johan.becue@sitecore.com>
2025-02-21 11:10:09 +0100

    modified:   headapps/nextjs-starter/src/components/SolutionEngineering/PageHeader.tsx

--- headapps/nextjs-starter/src/components/SolutionEngineering/PageHeader.tsx
+++ headapps/nextjs-starter/src/components/SolutionEngineering/PageHeader.tsx
@@ -102,7 +102,9 @@ export const ClassicLarge = (props: PageHeaderProps): JSX.Element => {
                 className="page-header-title-border visible"
                 style={{ width: '216.325px' }}
               ></span>
-              <h1 data-title-border="">Classic Large</h1>
+              <h1 data-title-border="">
+                <JssRichText field={props.fields.Title} />
+              </h1>
             </div>
           </div>
         </div>

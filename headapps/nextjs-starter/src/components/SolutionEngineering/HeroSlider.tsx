4c2079021c33c85e05e16e5cb73fc621c7af06e8
sc-johanbecue <johan.becue@sitecore.com>
2025-03-03 14:51:18 +0100

    modified:   headapps/nextjs-starter/src/components/SolutionEngineering/HeroSlider.tsx

--- headapps/nextjs-starter/src/components/SolutionEngineering/HeroSlider.tsx
+++ headapps/nextjs-starter/src/components/SolutionEngineering/HeroSlider.tsx
@@ -42,25 +42,10 @@ export const Default = (props: HeroSliderProps): JSX.Element => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          infinite: true,
+          dots: true,
         },
       },
     ],

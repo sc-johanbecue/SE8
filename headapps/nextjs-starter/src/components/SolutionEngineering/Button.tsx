e3a3f638c23940f08fedfa810af640a8cd942bae
sc-johanbecue <johan.becue@sitecore.com>
2025-03-06 01:56:24 +0100

    modified:   headapps/nextjs-starter/src/components/SolutionEngineering/Button.tsx

--- headapps/nextjs-starter/src/components/SolutionEngineering/Button.tsx
+++ headapps/nextjs-starter/src/components/SolutionEngineering/Button.tsx
@@ -103,3 +103,33 @@ export const Default = (props: ButtonProps): JSX.Element => {
 
   return <ButtonDefaultComponent {...props} />;
 };
+
+export const DefaultNotGood = (props: ButtonProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  // Create a ref to get the underlying <a> element.
+  const linkRef = useRef<HTMLAnchorElement>(null);
+
+  useEffect(() => {
+    // If the link should be disabled, set its attribute without a value.
+    if (linkRef.current && props.params.Disabled) {
+      linkRef.current.setAttribute('disabled', '');
+    }
+    // Assign the container's data-className attribute value to the link's className.
+  }, [props.params.Disabled, props.params.styles]);
+
+  if (props.fields) {
+    return (
+      <JssLink
+        ref={linkRef}
+        id={id ? id : undefined}
+        defaultValue="GET STARTED NOW!"
+        field={props.fields.Link}
+        className={`btn font-weight-bold text-3 py-3 btn-px-5 mt-1 ${props.params.styles}`}
+        style={{ animationDelay: '1800ms' }}
+      />
+    );
+  }
+
+  return <ButtonDefaultComponent {...props} />;
+};

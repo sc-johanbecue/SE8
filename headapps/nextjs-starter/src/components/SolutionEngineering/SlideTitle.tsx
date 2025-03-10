992c02671ec744a5b8af8df6ecbbf9ed37047612
sc-johanbecue <johan.becue@sitecore.com>
2025-03-10 01:57:46 +0100

    modified:   headapps/nextjs-starter/src/components/SolutionEngineering/SlideTitle.tsx 	modified:   headapps/nextjs-starter/src/components/SolutionEngineering/SlideTitle.tsx

--- headapps/nextjs-starter/src/components/SolutionEngineering/SlideTitle.tsx
+++ headapps/nextjs-starter/src/components/SolutionEngineering/SlideTitle.tsx
@@ -4,7 +4,9 @@ import {
   Text,
   ComponentParams,
   ComponentRendering,
+  Placeholder,
 } from '@sitecore-jss/sitecore-jss-nextjs';
+import 'SE_Image';
 import 'animate.css';
 
 interface Fields {
@@ -59,6 +61,76 @@ export const Default = (props: SlideTitleProps): JSX.Element => {
             alt=""
             style={{ animationDelay: '250ms' }}
           />
+        </span>
+      </h3>
+    );
+  }
+
+  return <SlideTitleDefaultComponent {...props} />;
+};
+
+export const WithPrefixSuffixImage = (props: SlideTitleProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  if (props.fields) {
+    return (
+      <h3
+        id={id || undefined}
+        className="position-relative text-color-light text-4 line-height-5 font-weight-normal px-4 mb-2 appear-animation animated fadeInDownShorter appear-animation-visible"
+        data-appear-animation="fadeInDownShorter"
+        data-plugin-options="{'minWindowWidth': 0}"
+        style={{ animationDelay: '100ms' }}
+      >
+        <span className="position-absolute right-100pct top-50pct transform3dy-n50 opacity-3">
+          <img
+            src="img/slides/slide-title-border.png"
+            className="w-auto appear-animation animated fadeInLeftShorter appear-animation-visible"
+            data-appear-animation="fadeInLeftShorter"
+            data-appear-animation-delay="250"
+            data-plugin-options="{'minWindowWidth': 0}"
+            alt=""
+            style={{ animationDelay: '250ms' }}
+          />
+        </span>
+        <Text field={props.fields.Title} />
+        <span className="position-absolute left-100pct top-50pct transform3dy-n50 opacity-3">
+          <img
+            src="img/slides/slide-title-border.png"
+            className="w-auto appear-animation animated fadeInRightShorter appear-animation-visible"
+            data-appear-animation="fadeInRightShorter"
+            data-appear-animation-delay="250"
+            data-plugin-options="{'minWindowWidth': 0}"
+            alt=""
+            style={{ animationDelay: '250ms' }}
+          />
+        </span>
+      </h3>
+    );
+  }
+
+  return <SlideTitleDefaultComponent {...props} />;
+};
+
+export const WithPrefixSuffixPlaceholder = (props: SlideTitleProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const phKeyHeadingPrefix = `headingPrefix-${props.params.DynamicPlaceholderId}`;
+  const phKeyHeadingSuffix = `headingSuffix-${props.params.DynamicPlaceholderId}`;
+
+  if (props.fields) {
+    return (
+      <h3
+        id={id || undefined}
+        className="position-relative text-color-light text-4 line-height-5 font-weight-normal px-4 mb-2 appear-animation animated fadeInDownShorter appear-animation-visible"
+        data-appear-animation="fadeInDownShorter"
+        data-plugin-options="{'minWindowWidth': 0}"
+        style={{ animationDelay: '100ms' }}
+      >
+        <span className="position-absolute right-100pct top-50pct transform3dy-n50 opacity-3">
+          <Placeholder name={phKeyHeadingPrefix} rendering={props.rendering} />
+        </span>
+        <Text field={props.fields.Title} />
+        <span className="position-absolute left-100pct top-50pct transform3dy-n50 opacity-3">
+          <Placeholder name={phKeyHeadingSuffix} rendering={props.rendering} />
         </span>
       </h3>
     );

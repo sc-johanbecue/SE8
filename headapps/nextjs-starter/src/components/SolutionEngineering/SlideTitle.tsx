53106329405adb805915322f4bde69a2a2791e13
sc-johanbecue <johan.becue@sitecore.com>
2025-03-10 10:08:18 +0100

    modified:   headapps/nextjs-starter/src/components/SolutionEngineering/SlideTitle.tsx

--- headapps/nextjs-starter/src/components/SolutionEngineering/SlideTitle.tsx
+++ headapps/nextjs-starter/src/components/SolutionEngineering/SlideTitle.tsx
@@ -29,6 +29,27 @@ const SlideTitleDefaultComponent = (props: SlideTitleProps): JSX.Element => (
 
 export const Default = (props: SlideTitleProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+
+  if (props.fields) {
+    return (
+      <h3
+        id={id || undefined}
+        className="position-relative text-color-light text-4 line-height-5 font-weight-normal px-4 mb-2 appear-animation animated fadeInDownShorter appear-animation-visible"
+        data-appear-animation="fadeInDownShorter"
+        data-plugin-options="{'minWindowWidth': 0}"
+        style={{ animationDelay: '100ms' }}
+      >
+        <Text field={props.fields.Title} />
+      </h3>
+    );
+  }
+
+  return <SlideTitleDefaultComponent {...props} />;
+};
+
+export const WithPrefixSuffixImage = (props: SlideTitleProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
   const prefixImage: ImageField = props.params.PrefixImage
     ? JSON.parse(props.params.PrefixImage)
     : undefined;
@@ -74,48 +95,6 @@ export const Default = (props: SlideTitleProps): JSX.Element => {
             />
           </span>
         )}
-      </h3>
-    );
-  }
-
-  return <SlideTitleDefaultComponent {...props} />;
-};
-
-export const WithPrefixSuffixImage = (props: SlideTitleProps): JSX.Element => {
-  const id = props.params.RenderingIdentifier;
-
-  if (props.fields) {
-    return (
-      <h3
-        id={id || undefined}
-        className="position-relative text-color-light text-4 line-height-5 font-weight-normal px-4 mb-2 appear-animation animated fadeInDownShorter appear-animation-visible"
-        data-appear-animation="fadeInDownShorter"
-        data-plugin-options="{'minWindowWidth': 0}"
-        style={{ animationDelay: '100ms' }}
-      >
-        <span className="position-absolute right-100pct top-50pct transform3dy-n50 opacity-3">
-          <img
-            src="img/slides/slide-title-border.png"
-            className="w-auto appear-animation animated fadeInLeftShorter appear-animation-visible"
-            data-appear-animation="fadeInLeftShorter"
-            data-appear-animation-delay="250"
-            data-plugin-options="{'minWindowWidth': 0}"
-            alt=""
-            style={{ animationDelay: '250ms' }}
-          />
-        </span>
-        <Text field={props.fields.Title} />
-        <span className="position-absolute left-100pct top-50pct transform3dy-n50 opacity-3">
-          <img
-            src="img/slides/slide-title-border.png"
-            className="w-auto appear-animation animated fadeInRightShorter appear-animation-visible"
-            data-appear-animation="fadeInRightShorter"
-            data-appear-animation-delay="250"
-            data-plugin-options="{'minWindowWidth': 0}"
-            alt=""
-            style={{ animationDelay: '250ms' }}
-          />
-        </span>
       </h3>
     );
   }

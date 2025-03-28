8558dfadd0577929559a5a562e2fddcbefffb41b
sc-johanbecue <johan.becue@sitecore.com>
2025-03-28 01:56:25 +0100

    modified:   headapps/nextjs-starter/src/components/SolutionEngineering/Heading.tsx

--- headapps/nextjs-starter/src/components/SolutionEngineering/Heading.tsx
+++ headapps/nextjs-starter/src/components/SolutionEngineering/Heading.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import Image from 'next/image';
 import {
   TextField,
   Text,
   ComponentParams,
   ComponentRendering,
-  useComponentProps,
-  GetStaticComponentProps,
 } from '@sitecore-jss/sitecore-jss-nextjs';
-import {
-  RenderingConfigurationFields,
-  fetchRenderingConfiguration,
-  concatenateClassNames,
-} from './Utility/RenderingConfigurationUtils';
 import 'animate.css';
 
 interface Fields {
@@ -51,82 +43,9 @@ export const Default = (props: HeadingProps): JSX.Element => {
   return <HeadingDefaultComponent {...props} />;
 };
 
-//export const getServerSideProps: GetServerSideComponentProps
-export const getStaticProps: GetStaticComponentProps = async () => {
-  console.log('Starting getStaticProps');
-
-  // Specify the path for the GraphQL query and the fields you need.
-  const staticProps = await fetchRenderingConfiguration(
-    '/sitecore/content/default/templates/Presentation/Rendering Configurations/Heading Configuration',
-    [
-      'PrefixImage',
-      'PrefixOpacity',
-      'PrefixAnimation',
-      'PrefixAnimationDelay',
-      'PrefixAnimationIteration',
-      'PrefixAnimationSpeed',
-      'SuffixImage',
-      'SuffixOpacity',
-      'SuffixAnimation',
-      'SuffixAnimationDelay',
-      'SuffixAnimationIteration',
-      'SuffixAnimationSpeed',
-    ]
-  );
-
-  console.log(
-    ('getStaticProps - FieldName: PrefixImage' +
-      ' - Value: ' +
-      staticProps.PrefixImage?.value.src) as string
-  );
-  console.log('Ended getStaticProps');
-  return staticProps;
-};
-
 // Helper function to render a heading with a dynamic tag
 export const renderHeading = (props: HeadingProps, Tag: keyof JSX.IntrinsicElements): JSX.Element => {
-  const staticProps = useComponentProps<RenderingConfigurationFields>(props.rendering.uid);
   const id = props.params.RenderingIdentifier;
-
-  const prefixImageClassNames = concatenateClassNames(
-    staticProps?.RenderingConfigurationFields.PrefixAnimation,
-    staticProps?.RenderingConfigurationFields.PrefixAnimationDelay,
-    staticProps?.RenderingConfigurationFields.PrefixAnimationIteration,
-    staticProps?.RenderingConfigurationFields.PrefixAnimationSpeed
-  );
-
-  const suffixImageClassNames = concatenateClassNames(
-    staticProps?.RenderingConfigurationFields.SuffixAnimation,
-    staticProps?.RenderingConfigurationFields.SuffixAnimationDelay,
-    staticProps?.RenderingConfigurationFields.SuffixAnimationIteration,
-    staticProps?.RenderingConfigurationFields.SuffixAnimationSpeed
-  );
-
-  const prefixSpanClassNames = concatenateClassNames(
-    staticProps?.RenderingConfigurationFields.PrefixOpacity
-  );
-
-  const suffixSpanClassNames = concatenateClassNames(
-    staticProps?.RenderingConfigurationFields.SuffixOpacity
-  );
-
-  const prefixSpanStyle = {
-    ...(staticProps?.RenderingConfigurationFields.PrefixImage.value.width && {
-      width: `${staticProps.RenderingConfigurationFields.PrefixImage.value.width}px`,
-    }),
-    ...(staticProps?.RenderingConfigurationFields.PrefixImage.value.height && {
-      height: `${staticProps.RenderingConfigurationFields.PrefixImage.value.height}px`,
-    }),
-  };
-
-  const suffixSpanStyle = {
-    ...(staticProps?.RenderingConfigurationFields.SuffixImage.value.width && {
-      width: `${staticProps.RenderingConfigurationFields.SuffixImage.value.width}px`,
-    }),
-    ...(staticProps?.RenderingConfigurationFields.SuffixImage.value.height && {
-      height: `${staticProps.RenderingConfigurationFields.SuffixImage.value.height}px`,
-    }),
-  };
 
   if (props.fields) {
     return (
@@ -134,35 +53,7 @@ export const renderHeading = (props: HeadingProps, Tag: keyof JSX.IntrinsicEleme
         id={id ? id : undefined}
         className={`component position-relative ${props.params.Styles}`}
       >
-        {staticProps?.RenderingConfigurationFields.PrefixImage && (
-          <span
-            className={`position-absolute right-100pct top-50pct transform3dy-n50 ${prefixSpanClassNames}`}
-            style={prefixSpanStyle}
-          >
-            <Image
-              className={`w-auto ${prefixImageClassNames}`}
-              src={staticProps?.RenderingConfigurationFields.PrefixImage.value.src}
-              alt={staticProps?.RenderingConfigurationFields.PrefixImage.value.alt as string}
-              fill={true}
-              sizes={`${staticProps?.RenderingConfigurationFields.PrefixImage.value.width}px`}
-            />
-          </span>
-        )}
         <Text field={props.fields.Title} />
-        {staticProps?.RenderingConfigurationFields.SuffixImage && (
-          <span
-            className={`position-absolute left-100pct top-50pct transform3dy-n50 ${suffixSpanClassNames}`}
-            style={suffixSpanStyle}
-          >
-            <Image
-              className={`w-auto ${suffixImageClassNames}`}
-              src={staticProps?.RenderingConfigurationFields.SuffixImage.value.src}
-              alt={staticProps?.RenderingConfigurationFields.SuffixImage.value.alt as string}
-              fill={true}
-              sizes={`${staticProps?.RenderingConfigurationFields.SuffixImage.value.width}px`}
-            />
-          </span>
-        )}
       </Tag>
     );
   }

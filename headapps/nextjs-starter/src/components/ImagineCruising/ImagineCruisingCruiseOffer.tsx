81286b7099b95ce274d211e0f09e14ab7c4e32ce
sc-johanbecue <johan.becue@sitecore.com>
2025-04-17 02:51:14 +0200

    new file:   headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingCruiseOffer.tsx 	new file:   headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingCruiseOffers.tsx

---
+++ headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingCruiseOffer.tsx
@@ -0,0 +1,102 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import {
+  Text,
+  TextField,
+  Field,
+  ImageField,
+  LinkField,
+  Image as JssImage,
+  Link as JssLink,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Name: TextField;
+  Exclusive: TextField;
+  Image: ImageField;
+  Favorite: Field;
+  Link: LinkField;
+  HolidayType: TextField;
+  Date: TextField;
+  Nights: TextField;
+  Operator: TextField;
+  Price: TextField;
+  PriceUnit: TextField;
+}
+
+type CruiseOfferProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+const CruiseOfferDefaultComponent = (props: CruiseOfferProps): JSX.Element => (
+  <div className={`component CruiseOffer ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">CruiseOffer</span>
+    </div>
+  </div>
+);
+
+export const Default = (props: CruiseOfferProps): JSX.Element => {
+  const id = 'CruiseOffer-content'; //props.params.RenderingIdentifier;
+
+  if (props.fields) {
+    return (
+      <div className="item-cruise cruise-719572 slide-position-1" id={id ? id : undefined}>
+        <JssLink field={props.fields.Link} className="test-cg cover-link" />
+        <div className="cruise-img-block">
+          <JssImage field={props.fields.Image} />
+          <div className="cruise-exclusive exclusive">
+            <span>
+              <Text field={props.fields.Exclusive} />
+            </span>
+          </div>
+          <span className="fave" data-favourite="719572">
+            <i className="far fa-heart"></i>
+          </span>
+        </div>
+        <div className="cruise-content-block">
+          <div className="cruise-top-content">
+            <div data-ga-element="cruise-offers-single-name" className="cruise-name">
+              All-Inclusive Ultimate South America Voyage, Machu Picchu &#038; Iguazu Falls
+            </div>
+            <div className="cruise-meta-block">
+              <div className="holiday-type">
+                <img src="https://www.imaginecruising.co.uk/wp-content/themes/imagine-rebrand/images/luggage.svg" />
+                <Text field={props.fields.HolidayType} />
+              </div>
+              <div className="holiday-dates">
+                <img src="https://www.imaginecruising.co.uk/wp-content/themes/imagine-rebrand/images/calendar-icon.svg" />
+                <Text field={props.fields.Date} /> | <Text field={props.fields.Nights} /> Nights
+              </div>
+              <div className="holiday-operator">
+                <img src="https://www.imaginecruising.co.uk/wp-content/themes/imagine-rebrand/images/ship.svg" />
+                <Text field={props.fields.Operator} />
+              </div>
+            </div>
+          </div>
+          <div className="cruise-button-block">
+            <div className="price-block">
+              <div className="cruise-price">
+                <span className="price-from">From</span>
+                <span className="the-price">
+                  &pound; <Text field={props.fields.Price} />
+                </span>
+                <span className="pp">
+                  {' '}
+                  <Text field={props.fields.PriceUnit} />
+                </span>
+              </div>
+            </div>
+            <JssLink
+              data-ga-element="cruise-offers-single-related-cta"
+              field={props.fields.Link}
+              className="default-cta"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return <CruiseOfferDefaultComponent {...props} />;
+};
---
+++ headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingCruiseOffers.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import {
+  TextField,
+  Text,
+  ComponentParams,
+  ComponentRendering,
+  Placeholder,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Title: TextField;
+  Text: TextField;
+}
+
+type CruiseOffersProps = {
+  rendering: ComponentRendering & { params: ComponentParams };
+  params: ComponentParams;
+  fields: Fields;
+};
+
+const CruiseOffersDefaultComponent = (props: CruiseOffersProps): JSX.Element => (
+  <div className={`component CruiseOffers ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">CruiseOffers</span>
+    </div>
+  </div>
+);
+
+export const Default = (props: CruiseOffersProps): JSX.Element => {
+  const id = 'CruiseOffers'; //props.params.RenderingIdentifier;
+  const phKey = `CruiseOffers-${props.params.DynamicPlaceholderId}`;
+
+  if (props.fields) {
+    return (
+      <section
+        id={id ? id : undefined}
+        className=" mobile-no-padding cruise-offers builder-section builder-cruise-offers  slider-position-0"
+      >
+        <div className="inner">
+          <h3 data-ga-element="cruise-offers-title">
+            <Text field={props.fields.Title} />
+          </h3>
+          <div className="cruises-block cruises-block-slider mobile-center-slider">
+            <Placeholder name={phKey} rendering={props.rendering} />
+          </div>
+        </div>
+      </section>
+    );
+  }
+  return <CruiseOffersDefaultComponent {...props} />;
+};

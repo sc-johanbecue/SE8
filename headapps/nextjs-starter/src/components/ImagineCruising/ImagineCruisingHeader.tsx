6c2295d409a164b61ba53e1e3baecddb8ac074c6
sc-johanbecue <johan.becue@sitecore.com>
2025-04-18 12:20:27 +0200

    modified:   headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingHeader.tsx

--- headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingHeader.tsx
+++ headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingHeader.tsx
@@ -100,12 +100,8 @@ export const Default = (props: HeaderProps): JSX.Element => {
             </div>
           </div>
         </div>
-        <div
-          data-ga-element="header-navigation-container"
-          className="header-nav-cont"
-          style={{ width: '100%' }}
-        >
-          <nav className="header-nav">
+        <div data-ga-element="header-navigation-container" className="header-nav-cont">
+          <nav className="header-nav" style={{ width: '100%' }}>
             <ul id="custom-nav" className="flex flex-justify-between flex-align-left">
               <li className="top-level " key="0">
                 <Link href="https://www.imaginecruising.co.uk/">Home</Link>

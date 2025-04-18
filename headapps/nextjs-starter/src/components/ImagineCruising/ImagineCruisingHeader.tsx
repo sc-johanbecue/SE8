0b540b9b639694cac903b505162dd09142fe895a
sc-johanbecue <johan.becue@sitecore.com>
2025-04-18 12:45:13 +0200

    modified:   headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingHeader.tsx

--- headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingHeader.tsx
+++ headapps/nextjs-starter/src/components/ImagineCruising/ImagineCruisingHeader.tsx
@@ -100,8 +100,8 @@ export const Default = (props: HeaderProps): JSX.Element => {
             </div>
           </div>
         </div>
-        <div className="header-nav-cont" style={{ width: '100%' }}>
-          <nav className="header-nav Johan" style={{ width: '100%' }}>
+        <div className="header-nav-cont" style={{ width: '100% !important' }}>
+          <nav className="header-nav Johan" style={{ width: '100% !important' }}>
             <ul id="custom-nav" className="flex flex-justify-between flex-align-left">
               <li className="top-level " key="0">
                 <Link href="https://www.imaginecruising.co.uk/">Home</Link>

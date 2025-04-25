45afc9cc0adcfa570338b3d3041b1246284fdff0
sc-johanbecue <johan.becue@sitecore.com>
2025-04-25 09:32:44 +0200

    modified:   headapps/nextjs-starter/src/components/SolutionEngineering/AnimatedLettersParagraph.tsx

--- headapps/nextjs-starter/src/components/SolutionEngineering/AnimatedLettersParagraph.tsx
+++ headapps/nextjs-starter/src/components/SolutionEngineering/AnimatedLettersParagraph.tsx
@@ -78,16 +78,16 @@ export const Default = (props: AnimatedLettersParagraphProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
 
   const headingClassNames = concatenateClassNames(
-    props.params.RenderingConfiguration.TextColor.fields.Value,
-    props.params.RenderingConfiguration.FontWeight.fields.Value,
-    props.params.RenderingConfiguration.PaddingStart.fields.Value,
-    props.params.RenderingConfiguration.PaddingEnd.fields.Value,
-    props.params.RenderingConfiguration.PaddingTop.fields.Value,
-    props.params.RenderingConfiguration.PaddingBottom.fields.Value,
-    props.params.RenderingConfiguration.MarginStart.fields.Value,
-    props.params.RenderingConfiguration.MarginEnd.fields.Value,
-    props.params.RenderingConfiguration.MarginTop.fields.Value,
-    props.params.RenderingConfiguration.MarginBottom.fields.Value
+    props.params.RenderingConfiguration.TextColor?.fields.Value,
+    props.params.RenderingConfiguration.FontWeight?.fields.Value,
+    props.params.RenderingConfiguration.PaddingStart?.fields.Value,
+    props.params.RenderingConfiguration.PaddingEnd?.fields.Value,
+    props.params.RenderingConfiguration.PaddingTop?.fields.Value,
+    props.params.RenderingConfiguration.PaddingBottom?.fields.Value,
+    props.params.RenderingConfiguration.MarginStart?.fields.Value,
+    props.params.RenderingConfiguration.MarginEnd?.fields.Value,
+    props.params.RenderingConfiguration.MarginTop?.fields.Value,
+    props.params.RenderingConfiguration.MarginBottom?.fields.Value
   );
 
   if (props.fields) {

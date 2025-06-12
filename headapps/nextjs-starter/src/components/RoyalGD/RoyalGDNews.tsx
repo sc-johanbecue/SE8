7cc5a12f3a37b5f119ae4921c15ea375e11d8653
sc-johanbecue <johan.becue@sitecore.com>
2025-06-12 07:28:54 +0200

    modified:   headapps/nextjs-starter/src/components/RoyalGD/RoyalGDNews.tsx

--- headapps/nextjs-starter/src/components/RoyalGD/RoyalGDNews.tsx
+++ headapps/nextjs-starter/src/components/RoyalGD/RoyalGDNews.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
+import {
+  TextField,
+  Text,
+  ImageField,
+  Image as JssImage,
+  RichTextField,
+  RichText,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface News {
   fields: {
-    Image: {
-      value: {
-        src: string;
-        alt?: string;
-      };
-    };
-    Title: {
-      value: string;
-    };
-    ShortText: {
-      value: string; // usually HTML
-    };
-    Text: {
-      value: string;
-    };
+    Image: ImageField;
+    Title: TextField;
+    ShortText: RichTextField;
+    Text: RichTextField;
   };
 }
 
 interface Fields {
-  Heading: {
-    value: string;
-  };
+  Heading: TextField;
   NewsList: News[];
   HighlightedNews: News;
 }
@@ -33,92 +28,93 @@ type HeaderProps = {
   fields: Fields;
 };
 
+const HeaderDefaultComponent = (props: HeaderProps): JSX.Element => (
+  <div className={`component Header ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">Header</span>
+    </div>
+  </div>
+);
+
 export const Default = (props: HeaderProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
 
   const newsList = props.fields.NewsList || [];
   const highlightedNews = props.fields.HighlightedNews || null;
 
-  return (
-    <>
-      <div key={id} id={id} className="row current-news">
-        <div className="columns small-12">
-          <h3>{props.fields.Heading?.value}</h3>
-          <hr />
-        </div>
+  if (props.fields) {
+    return (
+      <>
+        <div key={id ? id : undefined} id={id ? id : undefined} className="row current-news">
+          <div className="columns small-12">
+            <h3>
+              <Text field={props.fields.Heading} />
+            </h3>
+            <hr />
+          </div>
+          <div className="columns small-12 medium-6">
+            <div className="highlighted-item">
+              {highlightedNews ? (
+                <>
+                  <h3>
+                    <a
+                      id="phcolumn_0_phcontent_4_NieuwsTitelLink"
+                      href="https://www.gdanimalhealth.com/News/2025/06/Out-now-Update-magazine-May2025"
+                    >
+                      <Text field={props.fields.HighlightedNews.fields.Title} />
+                    </a>
+                  </h3>
+                  <div>
+                    <div id="phcolumn_0_phcontent_4_thumbnailDiv" className="thumbnail">
+                      <JssImage field={props.fields.HighlightedNews.fields.Image} />
+                    </div>
 
-        {/* Highlighted news */}
-        <div className="columns small-12 medium-6">
-          <div className="highlighted-item">
-            {highlightedNews && (
-              <>
-                <h3>
-                  <a
-                    id="phcolumn_0_phcontent_4_NieuwsTitelLink"
-                    href="https://www.gdanimalhealth.com/News/2025/06/Out-now-Update-magazine-May2025"
-                  >
-                    {highlightedNews.fields.Title?.value}
-                  </a>
-                </h3>
-                <div>
-                  <div id="phcolumn_0_phcontent_4_thumbnailDiv" className="thumbnail">
-                    <img
-                      src={highlightedNews.fields.Image?.value?.src}
-                      alt={highlightedNews.fields.Image?.value?.alt || ''}
-                      style={{ maxWidth: '100%' }}
-                    />
+                    <RichText field={props.fields.HighlightedNews.fields.ShortText} />
+                    <a
+                      id="phcolumn_0_phcontent_4_NieuwsDetailLink"
+                      className="readMore"
+                      href="https://www.gdanimalhealth.com/News/2025/06/Out-now-Update-magazine-May2025"
+                    >
+                      Read more
+                    </a>
                   </div>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: highlightedNews.fields.ShortText?.value || '',
-                    }}
-                  />
-                  <a
-                    id="phcolumn_0_phcontent_4_NieuwsDetailLink"
-                    className="readMore"
-                    href="https://www.gdanimalhealth.com/News/2025/06/Out-now-Update-magazine-May2025"
-                  >
-                    Read more
-                  </a>
-                </div>
-              </>
-            )}
+                </>
+              ) : null}
+            </div>
+          </div>
+          <div className="columns small-12 medium-6">
+            <ul className="list newsItemList list-table">
+              {newsList.map((news, index) => (
+                <li id={`ItemLi_${index}`} key={index}>
+                  <h4>
+                    <a
+                      id="phcolumn_0_phcontent_4_NieuwsRepeater_NieuwsDetailLink_0"
+                      href="https://www.gdanimalhealth.com/News/2025/05/Royal-GD-and-LeeO-announce-collaboration"
+                    >
+                      <Text field={news.fields.Title}></Text>
+                    </a>
+                  </h4>
+                  <span className="categorie" style={{ fontWeight: 'bold' }}></span>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-
-        {/* News list */}
-        <div className="columns small-12 medium-6">
-          <ul className="list newsItemList list-table">
-            {newsList.map((news, index) => (
-              <li id={`ItemLi_${index}`} key={index}>
-                <h4>
-                  <a
-                    id={`phcolumn_0_phcontent_4_NieuwsRepeater_NieuwsDetailLink_${index}`}
-                    href="https://www.gdanimalhealth.com/News/2025/05/Royal-GD-and-LeeO-announce-collaboration"
-                  >
-                    {news.fields.Title?.value}
-                  </a>
-                </h4>
-                <span className="categorie" style={{ fontWeight: 'bold' }}></span>
-              </li>
-            ))}
-          </ul>
+        <div className="row">
+          <div className="columns small-12 text-right">
+            <p>
+              <a
+                id="phcolumn_0_phcontent_5_Link"
+                className="button button-link large"
+                href="https://www.gdanimalhealth.com/News"
+              >
+                All news
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
-
-      <div className="row">
-        <div className="columns small-12 text-right">
-          <p>
-            <a
-              id="phcolumn_0_phcontent_5_Link"
-              className="button button-link large"
-              href="https://www.gdanimalhealth.com/News"
-            >
-              All news
-            </a>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+  return <HeaderDefaultComponent {...props} />;
 };

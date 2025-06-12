6b287159ed32efa907a8ed4d58a7c01328575ac1
sc-johanbecue <johan.becue@sitecore.com>
2025-06-12 07:21:09 +0200

    new file:   headapps/nextjs-starter/src/components/RoyalGD/RoyalGDNewsContent.tsx

---
+++ headapps/nextjs-starter/src/components/RoyalGD/RoyalGDNewsContent.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TextField, ImageField, RichTextField, Text, RichText, Image as JssImage } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Image: ImageField;
+  Title: TextField;
+  ShortText: TextField;
+  Text: RichTextField;
+}
+
+type HeaderProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+export const Default = (props: HeaderProps): JSX.Element => {
+
+
+  return (
+    <div>
+          <h1><Text field={props.fields.Title} /></h1>
+          <JssImage field={props.fields.Image} />
+          <br/>
+          <div>
+            <Text field={props.fields.ShortText} />
+          </div>
+          <br /><br /><br />
+          <div>
+            <RichText field={props.fields.Text} />
+          </div>
+    </div>
+  );
+};

6f94c9b332aef936b1fdd6e3a7dd46cb74802516
sc-johanbecue <johan.becue@sitecore.com>
2025-06-16 15:05:05 +0200

    modified:   headapps/nextjs-starter/src/pages/_app.tsx

--- headapps/nextjs-starter/src/pages/_app.tsx
+++ headapps/nextjs-starter/src/pages/_app.tsx
@@ -13,14 +13,14 @@ import Bootstrap from 'src/Bootstrap';
 // import '/src/assets/Geberit/geberit-tiles.scss';
 // import '/src/assets/sass/components/_component-promo.scss';
 
-// SOLUTIONENGINEERING
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-// import '/src/assets/SolutionEngineering/theme.css';
-// import '/src/assets/SolutionEngineering/theme-elements.css';
-// import '/src/assets/SolutionEngineering/theme-blog.css';
-// import '/src/assets/SolutionEngineering/theme-shop.css';
-// import '/src/assets/SolutionEngineering/custom.css';
-// import '/src/assets/SolutionEngineering/skins/skin-corporate-10.css';
+//SOLUTIONENGINEERING
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '/src/assets/SolutionEngineering/theme.css';
+import '/src/assets/SolutionEngineering/theme-elements.css';
+import '/src/assets/SolutionEngineering/theme-blog.css';
+import '/src/assets/SolutionEngineering/theme-shop.css';
+import '/src/assets/SolutionEngineering/custom.css';
+import '/src/assets/SolutionEngineering/skins/skin-corporate-10.css';
 
 // import 'assets/MajidAlFuttaim/main.css';
 function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
